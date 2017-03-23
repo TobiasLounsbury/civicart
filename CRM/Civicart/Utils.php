434fce4aff59981a20a88a172faf84deb107c613
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2017-03-23 16:53:06 -0700

    Added utility function for returning the urls of our needed resources so another plugin may include them when the 'normal' civi method is insufficient

--- CRM/Civicart/Utils.php
+++ CRM/Civicart/Utils.php
@@ -4,6 +4,9 @@
 class CRM_Civicart_Utils {
 
 
+  /**
+   * Hook to include Library Resources into a civi page.
+   */
   public static function addLibraryResources() {
     global $CiviCartLibraryIncluded;
 
@@ -16,5 +19,22 @@ class CRM_Civicart_Utils {
       $CiviCartLibraryIncluded = true;
     }
   }
+
+  /**
+   * Function to return a list of resource urls so they
+   * may be added by a helper function
+   */
+  public static function getLibraryResources() {
+
+    $resourceManager = CRM_Core_Resources::singleton();
+    $resources = array("js" => array(), "css" => array());
+
+    $resources['js'][] = $resourceManager->getUrl("com.tobiaslounsbury.civicart", "js/civicart.library.js");
+    $resources['css'][] = $resourceManager->getUrl("com.tobiaslounsbury.civicart", "css/civicart.css");
+
+    return $resources;
+  }
+
+
 
 }
